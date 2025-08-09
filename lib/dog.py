b9428cdd26e7e700fe689684fdb54599e792b558
kamaukimani <paulkimys285@gmail.com>
2025-08-09 07:25:40 +0300

    update

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
+
+# APPROVED_BREEDS = [
+#     "Mastiff",
+#     "Chihuahua",
+#     "Corgi",
+#     "Shar Pei",
+#     "Beagle",
+#     "French Bulldog",
+#     "Pug",
+#     "Pointer"
+# ]
+
+# class Dog:
+#     pass
+
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -12,4 +27,29 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Doggo", breed="Mastiff"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
+
